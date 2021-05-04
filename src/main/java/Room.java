d048c6b8fa86bce39043d6ecd587eb301c3aa293
Victor Saldanha Nunes <victorsnunes99@gmail.com>
2021-05-04 15:37:25 +0100

    walls and rooms created

---
+++ src/main/java/Room.java
@@ -0,0 +1,24 @@
+import java.util.ArrayList;
+import java.util.List;
+
+public class Room {
+
+    private List<Wall> walls = new ArrayList<>();
+
+    public Room(int x, int y, int width, int height) {
+
+        for (int i = x; i < x + width; i++) {
+            walls.add(new HorizontalWall(i, y));
+            walls.add(new HorizontalWall(i, y + height));
+        }
+
+        for (int j = y; j < y + height; j++) {
+            walls.add(new VerticalWall(x, j));
+            walls.add(new VerticalWall(x + width, j));
+        }
+    }
+
+    public List<Wall> getWalls() {
+        return walls;
+    }
+}
---
+++ src/main/java/VerticalWall.java
@@ -0,0 +1,15 @@
+import com.googlecode.lanterna.TerminalPosition;
+import com.googlecode.lanterna.TextColor;
+import com.googlecode.lanterna.graphics.TextGraphics;
+
+public class VerticalWall extends Wall {
+
+    public VerticalWall(Position position) { super(position); }
+    public VerticalWall(int x, int y) { super(x, y); }
+
+    @Override
+    public void draw(TextGraphics graphics) {
+        graphics.setForegroundColor(TextColor.Factory.fromString("#EF8433"));
+        graphics.putString(new TerminalPosition(getX(), getY()), "|");
+    }
+}
